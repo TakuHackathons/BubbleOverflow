a57b4f69142b22b00ce2bd7e1618791069441bbc
owner <owner@DESKTOP-SGQ9V7V>
2025-01-25 14:28:46 +0900

    bubble test

---
+++ Assets/Player.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private enum State
+    {
+        Stand,
+        Walk,
+        Pickup,
+        Hold,
+        Throw,
+        Put,
+        Damage
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
